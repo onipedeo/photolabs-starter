6d1e64e78c2797471441c9e82d1a3b4637144bf5
Tobi Onipede <onipede89@gmail.com>
2023-09-20 04:30:23 +0100

    add Fav button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,12 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+    <div className="photo-list__fav-icon" onClick={props.toggleFavorite}>
+      <div className="photo-list__fav-icon-svg"  >
+        <FavIcon selected={props.isFavorited} />
       </div>
     </div>
   );
