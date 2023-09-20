af197e642150de0152963ab44fa699f313324369
Tobi Onipede <onipede89@gmail.com>
2023-09-20 04:32:15 +0100

    add empty modal to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+// import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+
+import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [clickedPhoto, setClickedPhoto] = useState(null);
+
+  const openModalDetails = (photo) => {
+    setShowModal(true);
+    setClickedPhoto(photo)
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+    setClickedPhoto(null);
+  }
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <HomeRoute openModalDetails={openModalDetails} />
+      {showModal && <PhotoDetailsModal photo={clickedPhoto} closeModal={closeModal} />}
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/HomeRoute.scss';
+import TopNavigation from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
+
+
+  //Setting state for the favorited photos
+  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
+
+  //Function to toggle favorites
+  const toggleFavorite = (photoId) => {
+    // console.log("inside togglefav", photoId);
+    // console.log("favoritePhotos", favoritedPhotos);
+    if (favoritedPhotos.includes(photoId)) {
+      setFavoritedPhotos(favoritedPhotos.filter((id) => id !== photoId));
+    } else {
+      setFavoritedPhotos([...favoritedPhotos, photoId]);
+    }
+  }
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation topic={topics} favoritedPhotos={favoritedPhotos} />
+      <PhotoList photos={photos} toggleFavorite={toggleFavorite} favoritedPhotos={favoritedPhotos} openModalDetails={props.openModalDetails} />
     </div>
   );
 };
