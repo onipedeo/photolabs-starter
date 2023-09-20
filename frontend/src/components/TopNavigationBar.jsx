300d882b76d1f2872e45dbc438559a5b588c25c5
Tobi Onipede <onipede89@gmail.com>
2023-09-20 04:28:37 +0100

    add Top navigation bar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import '../styles/TopNavigationBar.scss'
+import FavBadge from './FavBadge';
+import TopicList from './TopicList';
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
+  const { favoritedPhotos } = props;
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span className="top-nav-bar__logo">
+        PhotoLabs
+      </span>
+      <TopicList topics={props.topic} />
+      <FavBadge isFavPhotoExist={favoritedPhotos.length > 0} />
+
     </div>
   )
 }
