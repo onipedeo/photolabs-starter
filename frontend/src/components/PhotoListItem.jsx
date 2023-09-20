560b9e71ce6851ec3a30ca315f5df483ed8b8a8b
Tobi Onipede <onipede89@gmail.com>
2023-09-20 04:29:46 +0100

    add Photlist and PhotolistItem Component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  const { id, location, urls, user } = props.photo;
+  const { favoritedPhotos, toggleFavorite } = props;
+
+  const onClick = () => {
+    props.openModalDetails(props.photo);
+    console.log(props.photo);
+  }
+
+  return (
+    <>
+      <div className="photo-list__item">
+        <PhotoFavButton isFavorited={favoritedPhotos.includes(id)} toggleFavorite={() => toggleFavorite(id)} />
+        <img className="photo-list__image" src={urls.regular}
+          onClick={onClick} alt="image of Photo" />
+
+        <div className="photo-list__user-details">
+          <img className="photo-list__user-profile" src={user.profile} alt="profile image" />
+          <div className="photo-list__user-info ">
+            {user.name}
+            <div className="photo-list__user-location" >
+              {`${location.city}, ${location.country}`}
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
 };
 
 export default PhotoListItem;
