394a3429391ff07251e70dd5a16f2e88de45f158
Tobi Onipede <onipede89@gmail.com>
2023-09-22 05:54:53 +0100

    implement useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,47 +1,64 @@
 import { React, useState } from "react";
+import { useReducer } from "react";
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
+
+const initialState = {
+  displayModal: false,
+  favoritedPhotos: [],
+  clickedPhoto: null
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return { ...state, favoritedPhotos: [...state.favoritedPhotos, action.payload] };
+
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      return { ...state, favoritedPhotos: state.favoritedPhotos.filter(id => id !== action.payload) };
+
+    case ACTIONS.SELECT_PHOTO:
+      return { ...state, displayModal: true, clickedPhoto: action.payload };
+
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      return { ...state, displayModal: false, clickedPhoto: null };
+
+    default:
+      return state;
+  }
+};
 
 const useApplicationData = function() {
-  const [state, setState] = useState({
-    displayModal: false,
-    favoritedPhotos: [],
-    clickedPhoto: null
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const updateToFavPhotoIds = (photoId) => {
     if (state.favoritedPhotos.includes(photoId)) {
-      const copyOfFavorites = [...state.favoritedPhotos].filter((id) => id !== photoId);
-      setState({
-        ...state,
-        favoritedPhotos: copyOfFavorites
-
-      })
-      return;
+      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: photoId });
+    } else {
+      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: photoId });
     }
-    setState({
-      ...state,
-      favoritedPhotos: [...state.favoritedPhotos, photoId]
-    })
-  }
+  };
 
   const setPhotoSelected = (openModalDetails) => {
-    setState({
-      ...state,
-      displayModal: true,
-      clickedPhoto: openModalDetails
-    })
-  }
+    dispatch({ type: ACTIONS.SELECT_PHOTO, payload: openModalDetails });
+  };
 
   const onClosePhotoDetailsModal = () => {
-    setState({
-      ...state,
-      displayModal: false,
-      clickedPhoto: null
-    })
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS });
   }
 
   return {
-    state, updateToFavPhotoIds, setPhotoSelected, onClosePhotoDetailsModal
-
+    state,
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal
   }
 
 }
